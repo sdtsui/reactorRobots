881a97ee64c97d994e800351971eadf5b2ccb951
sdtsui <danielt213@gmail.com>
2015-03-13 10:13:55 -0700

    reorganized to hold egghead tutorials, will turn into a toy problem folder

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/',express.static(__dirname+"/client"));
+app.use('/',express.static(__dirname+"/eggheadPlay"));
 
 var port = process.env.PORT || 8080;
 console.log("Listening on Port : " ,port);
